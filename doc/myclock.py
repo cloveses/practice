e36d4a4bb5654215d15d001c8b46b0d2e777f735
cloveses <cloveses@163.com>
2018-08-20 11:07:48 +0800

    m

--- doc/myclock.py
+++ doc/myclock.py
@@ -1,5 +1,9 @@
 import math
+import time
 import itertools
+import tkinter
+import threading
+
 
 def get_all_points(center_point,plong):
     end_points = []
@@ -25,7 +29,7 @@ class MyLine:
 
     def delete(self):
         if self.widget_id:
-            self.cavans.delete(self. widget_id)
+            self.canvas.delete(self.widget_id)
 
 class Pointer(MyLine):
 
@@ -35,18 +39,19 @@ class Pointer(MyLine):
         self.plong = plong
         self.ptype = ptype
         self.end_point = None
-        self.points = itertools.cycle(gen_end_points(self.center_point,self.plong,sep))
+        self.points = itertools.cycle(gen_end_points(self.center_point,self.plong,self.ptype))
+        self.count,self.end_point = self.points.__next__()
 
     def draw(self):
-        self.count,self.end_point = self.points.__next__()
         self.widget_id = self.canvas.create_line(self.center_point,self.end_point, 
             width=self.width, fill=self.color)
+        self.count,self.end_point = self.points.__next__()
 
     def walk(self):
         self.delete()
         self.draw()
 
-class Maker(MyLine):
+class Marker(MyLine):
 
     def __init__(self,start_point,end_point,canvas,width=2,color='black'):
         super().__init__(canvas,width,color)
@@ -54,11 +59,62 @@ class Maker(MyLine):
         self.end_point = end_point
 
     def draw(self):
-        self.widget_id = self.canvas.create_line(self.center_point,self.end_point, 
+        self.widget_id = self.canvas.create_line(self.start_point,self.end_point, 
             width=self.width, fill=self.color)
 
-class Plate():
+class Plate:
 
-    def __init__(self,center_point,radius,plong):
-        for start,end in zip(gen_end_points(center_point,radius,6),gen_end_points(center_point,radius + plong,6)):
-            
+    def __init__(self,canvas, center_point, radius, plong):
+        self.canvas = canvas
+        self.center_point = center_point
+        self.radius = radius
+        self.plong = plong
+        self.markers = []
+        self.gen_markers()
+
+    def draw(self):
+        for marker in self.markers:
+            marker.draw()
+
+    def gen_markers(self):
+        for start,end in zip(gen_end_points(self.center_point,self.radius,6),
+                gen_end_points(self.center_point,self.radius + self.plong,6)):
+            self.markers.append(Marker(start[1],end[1],self.canvas))
+
+class MyClocker:
+
+    def __init__(self,root,canvas, center_point, radius, plong):
+        self.root = root
+        self.plate = Plate(canvas, center_point, radius, plong)
+        self.s_pointer = Pointer(6,canvas,center_point,plong=180,width=1,color='red')
+        self.m_pointer = Pointer(3,canvas,center_point,plong=150,width=2,color='black')
+        self.h_pointer = Pointer(1,canvas,center_point,plong=120,width=4,color='black')
+        self.plate.draw()
+        self.s_pointer.draw()
+        self.m_pointer.draw()
+        self.h_pointer.draw()
+        self.root.update()
+
+    def walk(self):
+        self.s_pointer.walk()
+        if self.s_pointer.count + 1 % 30 == 0:
+            self.m_pointer.walk()
+        if self.m_pointer.count + 1 % 4 == 0:
+            self.h_pointer.walk()
+
+    def start(self):
+        while True:
+            self.walk()
+            self.root.update()
+            time.sleep(1)
+
+if __name__ == '__main__':
+    root = tkinter.Tk()
+    cvns = tkinter.Canvas(root,width=530,height=530,bg='white')
+    cvns.pack()
+    mc = MyClocker(root,cvns,(260,260),200,10)
+    t = threading.Thread(target=mc.start)
+    t.setDaemon(True)
+    t.start()
+    root.resizable(False, False)
+    root.mainloop()
