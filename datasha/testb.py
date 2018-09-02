209095d1053be8d2fc26e26968242a62ffa4b5f0
djx <cloveses@126.com>
2018-09-02 14:34:09 +0800

    Update testb.py

--- datasha/testb.py
+++ datasha/testb.py
@@ -8,7 +8,8 @@ def getdata(filename):
         print('File is not exists!')
         return
     i = -1
-    rets = [0,] * 40
+    rets = [0,] * 40 #用于保存结果
+    # 循环获取
     for line in fileinput.input(filename):
         i += 1
         if i % 41==0:
@@ -17,25 +18,27 @@ def getdata(filename):
             data = float(line.split(' ')[-1])
             rets[i%41 - 1] += data
     n = i // 41 + 1
-    rets = [r/n for r in rets]
+    rets = [round(r/n,4) for r in rets]
     print(rets)
     return rets
 
 def tofile(data,filename):
+    #写入文件
     with open(filename,'wt') as f:
-        for d in data:
-            f.write(str(d))
+        for i,d in enumerate(data):
+            f.write(' '.join((str(i+1),str(d))))
             f.write('\n')
             
 if __name__ == '__main__':
-    inputf = 'abc.txt'
-    outputf = 'result.txt'
-#     print(len(sys.argv),sys.argv)
+    # 可以在命令行提供输入和输出文件名
+    inputf = 'abc.txt' #默认输入文件名
+    outputf = 'result.txt' #默认输出文件名
+##    print(len(sys.argv),sys.argv)
     if len(sys.argv) >= 2:
         inputf = sys.argv[1]
     if len(sys.argv) >= 3:
         outputf = sys.argv[2]
         
-#     print(inputf,outputf)
+##    print(inputf,outputf)
     data = getdata(inputf)
     tofile(data,outputf)
