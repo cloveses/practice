7e5831acd30f009d3397ef3a1b30982c7036f92d
cloveses <cloveses@163.com>
2018-10-08 17:38:24 +0800

    m

--- aiotest/main.py
+++ aiotest/main.py
@@ -1,8 +1,9 @@
 from aiohttp import web
+import aiohttp_mako
 import logging
 
-# access_log = logging.getLogger('aiohttp.access')
-# logging.basicConfig(level=logging.DEBUG)
+access_log = logging.getLogger('aiohttp.access')
+logging.basicConfig(level=logging.DEBUG)
 routes = web.RouteTableDef()
 
 @routes.get('/')
@@ -18,9 +19,17 @@ async def phello(request):
     print('aaabb')
     return web.json_response({'status':0,'data':{'a':2,'b':2}})
 
+@aiohttp_mako.template('index.html')
+@routes.get('/mako')
+async def mako_test(request):
+    return {'aa':'22'}
 
 app = web.Application(debug=True)
 app.add_routes(routes)
-for rsrc in app.router.resources():
-    print(rsrc)
+# for rsrc in app.router.resources():
+#     print(rsrc)
+lookup = aiohttp_mako.setup(app, input_encoding='utf-8',
+    output_encoding='utf-8',
+    default_filters=['decode.utf8'])
+lookup.put_string('index.html','''<html><body><h1>${aa}</h1>${text}</body></html>''')
 web.run_app(app)
